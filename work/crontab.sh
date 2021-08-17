6bcb16b1f935f62c4bd04217e0e737ef9e168dca
haoduck <56804964+haoduck@users.noreply.github.com>
2021-08-18 07:14:47 +0800

    fix $rand_time

--- work/crontab.sh
+++ work/crontab.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-rand_time=$(rand_time=$(min=1000;max=9999;expr $(date +%N) %  $(($max - $min  + 1)) + $min))
+min=1000
+max=9999
+rand_time=$(($RANDOM*$RANDOM%$(($max-$min+1))+$min))
 echo "sleep ${rand_time}s"
 sleep ${rand_time}s
 python /work/index.py
