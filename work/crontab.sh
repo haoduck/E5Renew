14d0e69287bd4d97122e3d1f26899710c2427176
haoduck <56804964+haoduck@users.noreply.github.com>
2021-08-18 08:30:13 +0800

    优化日志输出

--- work/crontab.sh
+++ work/crontab.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+startDate=`date +"%Y-%m-%d %H:%M:%S"`
+echo "Start: [$startDate]——————sleep ${rand_time}s"
+
 min=1000
 max=9999
 rand_time=$(($RANDOM*$RANDOM%$(($max-$min+1))+$min))
-echo "sleep ${rand_time}s"
 sleep ${rand_time}s
 python /work/index.py
+
+echo "----------------------------------------------------------------------------"
+endDate=`date +"%Y-%m-%d %H:%M:%S"`
+echo "★[$endDate] Successful"
+echo "----------------------------------------------------------------------------"
